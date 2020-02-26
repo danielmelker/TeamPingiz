22b1dd519d1dd65b71bec547e442e02e379f1d05
Elvira <elviramagnusson96@gmail.com>
2020-02-26 13:39:11 +0100

    Created booking files

--- src/main/java/com/example/demo/booking/BookingController.java
+++ src/main/java/com/example/demo/booking/BookingController.java
@@ -1,4 +1,17 @@
 package com.example.demo.booking;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class BookingController {
+
+    @GetMapping("/booking")
+    String getBooking(){
+        return "booking";
+    }
+
+    @PostMapping("/booking")
+    String booking(){
+     return "booking";
+    }
 }
