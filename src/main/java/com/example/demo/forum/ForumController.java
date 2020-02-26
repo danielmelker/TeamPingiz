c817cda2626a417b0aaaf151311203345e1ebdd2
Christoffer Deurell <christoffer.deurell@live.se>
2020-02-26 14:02:46 +0100

    forum controller added + html

--- src/main/java/com/example/demo/forum/ForumController.java
+++ src/main/java/com/example/demo/forum/ForumController.java
@@ -1,4 +1,24 @@
 package com.example.demo.forum;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ForumController {
+
+    @GetMapping("/forum")
+    public String showForum(){
+
+        return "forum";
+    }
+
+    @PostMapping("/forum")
+    public String postToForum(@RequestParam String inputText, Model model){
+
+        model.addAttribute("textAtt",inputText);
+        return "forum";
+    }
 }
