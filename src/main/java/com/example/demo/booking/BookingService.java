c7aaf6fc5cbf66f1c372897db28bd22ee91ce62a
Jacob Winding <jacobkwinding@gmail.com>
2020-02-27 14:02:31 +0100

    fixed bookedBy user functionality

--- src/main/java/com/example/demo/booking/BookingService.java
+++ src/main/java/com/example/demo/booking/BookingService.java
@@ -1,5 +1,6 @@
 package com.example.demo.booking;
 
+import com.example.demo.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +18,11 @@ public class BookingService {
         this.bookingRep = bookingRep;
     }
 
-    public void setToBooked(int slotID) {
+    public void setToBooked(int slotID, User booker) {
         for(BookingSlot slot : bookingRep.getBookingSlotList()){
             if(slot.getSlotID() == slotID){
                 slot.setAvailable(false);
+                slot.setBookedBy(booker);
             }
         }
     }
