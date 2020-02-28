a82db5d9064b1b71459faa63ac963cdb19c30fa3
Jacob Winding <jacobkwinding@gmail.com>
2020-02-28 09:35:33 +0100

    added functionality for seeing what slots a user has booked

--- src/main/java/com/example/demo/TeamPingizController.java
+++ src/main/java/com/example/demo/TeamPingizController.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import com.example.demo.booking.BookingService;
+import com.example.demo.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
@@ -10,11 +14,16 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class TeamPingizController {
 
-
+    @Autowired
+    BookingService bookingService;
 
     @GetMapping
-    public String showIndex(HttpSession session) {
+    public String showIndex(HttpSession session, Model model) {
         if (session.getAttribute("validated") != null && (boolean) session.getAttribute("validated")) {
+
+            User user = (User)session.getAttribute("user");
+            var slots = bookingService.getSlotsBookedBy(user);
+            model.addAttribute("bookedSlots", slots);
             return "index";
         } else {
             return "landingPage";
--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -34,10 +34,9 @@ public class UserController {
         Stream.of(u,u1,u2,u3,u4,u5).forEach(user -> user.setDescription("Pingiz developer and ping-pong master!")) ;
         u4.setFileURL("https://upload.wikimedia.org/wikipedia/commons/5/53/Donald_Trump_official_portrait_%28cropped%29.jpg");
         u4.setDescription("Make Pingis Great Again!");
+
+        u5.setFileURL("https://media-exp1.licdn.com/dms/image/C4D03AQHAuUy7IJsm_w/profile-displayphoto-shrink_200_200/0?e=1586390400&v=beta&t=_5Q0DcIwGkMulRZaIISS775N1-WgP4u9xawCU0C-Y_Y");
         userService.register(u,u1,u2,u3,u4,u5);
-
-
-
     }
 
 
