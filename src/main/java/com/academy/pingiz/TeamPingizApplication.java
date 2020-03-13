241a082a18239ffb4aae36f855594f87dbd618e9
Christoffer Deurell <christoffer.deurell@live.se>
2020-03-13 10:40:50 +0100

    rätt timezone

--- src/main/java/com/academy/pingiz/TeamPingizApplication.java
+++ src/main/java/com/academy/pingiz/TeamPingizApplication.java
@@ -3,10 +3,17 @@ package com.academy.pingiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class TeamPingizApplication {
 
+
+
+
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Stockholm"));
 		SpringApplication.run(TeamPingizApplication.class, args);
 	}
 
